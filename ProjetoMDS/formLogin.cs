a417d25e573e49dc772060421dbcaa6703ce0fd7
André Paulo <andre_duivenvoorden@hotmail.com>
2017-05-05 22:51:28 +0100

    login e mostra menu

--- ProjetoMDS/formLogin.cs
+++ ProjetoMDS/formLogin.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Windows.Forms;
 using MySql.Data.MySqlClient;
+using System.Text;
 
 namespace ProjetoMDS {
     public partial class formLogin : Form {
@@ -11,26 +12,41 @@ namespace ProjetoMDS {
         private void button1_Click(object sender, EventArgs e) {
             string connetionString = null;
             MySqlConnection cnn;
-            connetionString = "server=andrepaulo.me;database=mds-projeto;uid=mds;pwd=fBLeaLfdHicXSY0T;";
+            formMenu formMenu = new formMenu();
+            connetionString = "server=localhost;database=mds-projeto;uid=root;pwd=;";
             cnn = new MySqlConnection(connetionString);
             try {
-                cnn.Open();
-                MessageBox.Show("Connection Open ! ");
-                
+                cnn.Open();                
                 MySqlCommand query = cnn.CreateCommand();
-                query.CommandText = "SELECT id FROM users WHERE username = @username AND password = @password";
+                query.CommandText = "SELECT id FROM users WHERE username = @username AND password = @password LIMIT 1";
                 query.Parameters.AddWithValue("@username", tbNome.Text);
-                query.Parameters.AddWithValue("@password", tbPassword.Text);
+                query.Parameters.AddWithValue("@password", hashpassword(tbPassword.Text));
                 MySqlDataReader reader = query.ExecuteReader();
                 string id = "";
                 while(reader.Read()) {
                     id = reader.GetString(0);
                 }
-                tbNome.Text=id;
+                if(id == "") {
+                    MessageBox.Show("Erro. Credenciais inválidas.");
+                } else {
+                    formMenu.Show();
+                    Hide();
+                }
                 cnn.Close();
             } catch(Exception ex) {
-                MessageBox.Show("Can not open connection ! "+ex);
+                MessageBox.Show("Erro na conexão ao servidor MySQL \n"+ex);
             }
+        }
+
+        // http://stackoverflow.com/a/14709940/6195472
+        public string hashpassword(string password) {
+            System.Security.Cryptography.SHA256Managed crypt = new System.Security.Cryptography.SHA256Managed();
+            System.Text.StringBuilder hash = new System.Text.StringBuilder();
+            byte[] crypto = crypt.ComputeHash(Encoding.UTF8.GetBytes(password), 0, Encoding.UTF8.GetByteCount(password));
+            foreach(byte theByte in crypto) {
+                hash.Append(theByte.ToString("x2"));
+            }
+            return hash.ToString();
         }
     }
 }
