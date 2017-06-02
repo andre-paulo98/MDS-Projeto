b8bd5eeee6f9e31aaffcfa0f58a48e9ab7a5dc3e
Andre Carvalho <andre_r_carvalho@hotmail.com>
2017-06-02 10:06:26 +0100

    Atualização da Base de Dados

--- mds-projeto.sql
+++ mds-projeto.sql
@@ -1,40 +1,91 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: 02-Jun-2017 às 11:04
+-- Versão do servidor: 10.1.21-MariaDB
+-- PHP Version: 7.0.9
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
+--
+-- Database: `mds-projeto`
+--
 DROP DATABASE IF EXISTS `mds-projeto`;
-
-CREATE DATABASE `mds-projeto` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-
+CREATE DATABASE IF NOT EXISTS `mds-projeto` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 USE `mds-projeto`;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `medico`
+--
+
+CREATE TABLE `medico` (
+  `id` int(11) NOT NULL,
+  `especialidade` int(11) NOT NULL,
+  `entrada` time NOT NULL,
+  `saida` time NOT NULL,
+  `segSocial` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `users`
+--
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `username` varchar(50) NOT NULL,
   `password` varchar(64) NOT NULL,
-  `permissao` int(11) NOT NULL COMMENT '0-medico 1-rececionista 2-admin',
-  `cargo` varchar(20) NOT NULL,
-  `med_especialidade` varchar(50) DEFAULT NULL,
-  `med_horaentrada` time DEFAULT NULL,
-  `med_horasaida` time DEFAULT NULL,
-  `med_segSocial` int(11) DEFAULT NULL
+  `permissao` int(11) NOT NULL COMMENT '0-medico 1-rececionista 2-admin'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO `users` (`id`, `username`, `password`, `permissao`, `cargo`, `med_especialidade`, `med_horaentrada`, `med_horasaida`, `med_segSocial`) VALUES
-(1, 'teste', '46070d4bf934fb0d4b06d9e2c46e346944e322444900a435d7d9a95e6d7435f5', 2, 'Administrador', NULL, NULL, NULL, NULL);
+--
+-- Indexes for dumped tables
+--
 
+--
+-- Indexes for table `medico`
+--
+ALTER TABLE `medico`
+  ADD KEY `id` (`id`);
 
+--
+-- Indexes for table `users`
+--
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `username` (`username`);
 
+--
+-- AUTO_INCREMENT for dumped tables
+--
 
+--
+-- AUTO_INCREMENT for table `users`
+--
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Limitadores para a tabela `medico`
+--
+ALTER TABLE `medico`
+  ADD CONSTRAINT `medico_ibfk_1` FOREIGN KEY (`id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
